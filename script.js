a1f976f303457e90a8f79d4933bd59170f3ef3c1
eleanorex <141073216+eleanorex@users.noreply.github.com>
2024-02-14 23:24:36 +0200

    upd

--- script.js
+++ script.js
@@ -1,66 +1,16 @@
-function plusOperation() {
-    output.textContent += "+";
+function appendToDisplay(input) {
+    output.textContent += input;
 }
 
-function minusOperation() {
-    output.textContent += "-";
-}
-
-function multiplyOperation() {
-    output.textContent += "*";
-}
-
-function divideOperation() {
-    output.textContent += "/";
-}
-
-function clearOperation() {
+function clearOutput() {
     output.textContent = "";
 }
 
-function addSevenOperation() {
-    output.textContent += "7";
-}
-
-function addFourOperation() {
-    output.textContent += "4";
-}
-
-function addOneOperation() {
-    output.textContent += "1";
-}
-
-function addZeroOperation() {
-    output.textContent += "0";
-}
-
-function addEightOperation() {
-    output.textContent += "8";
-}
-
-function addFiveOperation() {
-    output.textContent += "5";
-}
-function addTwoOperation() {
-    output.textContent += "2";
-}
-
-function addDotOperation() {
-    output.textContent += ".";
-}
-
-function addNineOperation() {
-    output.textContent += "9";
-}
-
-function addSixOperation() {
-    output.textContent += "6";
-}
-
-function addThreeOperation() {
-    output.textContent += "3";
-}
-
-function equalOperation() {
-    output.textContent = eval(output.textContent)
+function calculate() {
+    try {
+        output.textContent = eval(output.textContent)
+    }
+    catch(error) {
+        output.textContent = "Error"
+    }
 }
