63b7c154b63933eaa1d5d340027c2cae7522716d
eleanorex <141073216+eleanorex@users.noreply.github.com>
2024-02-14 22:55:28 +0200

    upd

--- script.js
+++ script.js
@@ -33,3 +33,34 @@ function addOneOperation() {
 function addZeroOperation() {
     output.textContent += "0";
 }
+
+function addEightOperation() {
+    output.textContent += "8";
+}
+
+function addFiveOperation() {
+    output.textContent += "5";
+}
+function addTwoOperation() {
+    output.textContent += "2";
+}
+
+function addDotOperation() {
+    output.textContent += ".";
+}
+
+function addNineOperation() {
+    output.textContent += "9";
+}
+
+function addSixOperation() {
+    output.textContent += "6";
+}
+
+function addThreeOperation() {
+    output.textContent += "3";
+}
+
+function equalOperation() {
+    output.textContent = eval(output.textContent);
+}
