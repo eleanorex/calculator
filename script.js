be438b8f7c95a271a8a7156298674c79abbfa105
eleanorex <141073216+eleanorex@users.noreply.github.com>
2024-02-14 22:46:18 +0200

    upd

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-const input = document.querySelector(".input");
+function plusOperation() {
+    output.textContent += "+";
+}
+
+function minusOperation() {
+    output.textContent += "-";
+}
+
+function multiplyOperation() {
+    output.textContent += "*";
+}
+
+function divideOperation() {
+    output.textContent += "/";
+}
+
+function clearOperation() {
+    output.textContent = "";
+}
+
+function addSevenOperation() {
+    output.textContent += "7";
+}
